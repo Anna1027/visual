413805e2128566d9d3f13dc318d5b377c083d510
Anna Joen <annajoen@Annas-MacBook-Air.local>
2019-08-07 10:44:57 -0400

    visualization

---
+++ scripts.py
@@ -0,0 +1,14 @@
+x = [1,2,'car',10,20]
+print (x)
+
+import numpy as np
+t = np.linspace(0,1,5)
+print (t)
+y = np.array([1,2,3.5,10,20])
+print (y)
+
+import matplotlib.pyplot as plt
+plt.plot(t,y, 'r.')
+plt.show()
+
+
